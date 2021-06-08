1a315d18163ca973561a837d54217750d848e71a
Agustin Souto <agus.souto75@gmail.com>
2021-06-08 12:50:57 -0300

    Created Slider with projects

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import {Carousel} from 'react-bootstrap'
+
+export default function Slider() {
+    return (
+        <Carousel fade className="carousel">
+            <Carousel.Item>
+                <img
+                className="d-block w-100"
+                src="https://github.com/Souto751/portfolio/blob/main/src/images/watchlist.jpg?raw=true"
+                alt="First slide" />
+                <Carousel.Caption className="text">
+                <h3>Watchlist</h3>
+                <p>This is a watchlist that displays all the series, movies and animes that I watched with my girlfriend.</p>
+                <div className="buttons">
+                    <a href="https://github.com/Souto751/watchlist" target="_blank" rel="noreferrer"><button className="btn btn-secondary">Repo</button></a>
+                    <a href="https://souto751.github.io/watchlist/" target="_blank" rel="noreferrer"><button className="btn btn-primary">Page</button></a>
+                </div>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                className="d-block w-100"
+                src="https://github.com/Souto751/portfolio/blob/main/src/images/food4wild.jpg?raw=true"
+                alt="Second slide"
+                />
+
+                <Carousel.Caption className="text">
+                <h3>Food4Wild</h3>
+                <p>This is a Product Landing Page, that sells food for wild animals. It's only an example.</p>
+                <div className="buttons">
+                    <a href="https://codepen.io/Souto751/pen/YzGeNKo" target="_blank" rel="noreferrer"><button className="btn btn-secondary">Repo</button></a>
+                    <a href="https://codepen.io/Souto751/full/YzGeNKo" target="_blank" rel="noreferrer"><button className="btn btn-primary">Page</button></a>
+                </div>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                className="d-block w-100"
+                src="https://github.com/Souto751/portfolio/blob/main/src/images/bakery.jpg?raw=true"
+                alt="Third slide"
+                />
+
+                <Carousel.Caption className="text">
+                <h3>Pupi's Bakery</h3>
+                <p>This is a web page for a Bakery.</p>
+                <div className="buttons">
+                    <a href="https://github.com/Souto751/pupiBakery" target="_blank" rel="noreferrer"><button className="btn btn-secondary">Repo</button></a>
+                    <a href="https://pupi-bakery.web.app/" target="_blank" rel="noreferrer"><button className="btn btn-primary">Page</button></a>
+                </div>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                className="d-block w-100"
+                src="https://github.com/Souto751/portfolio/blob/main/src/images/testbook.jpg?raw=true"
+                alt="Third slide"
+                />
+
+                <Carousel.Caption className="text">
+                <h3>testBook</h3>
+                <p>This is a social network, that is not working by the moment.</p>
+                <div className="buttons">
+                    <a href="https://github.com/Souto751/testbook" target="_blank" rel="noreferrer"><button className="btn btn-secondary">Repo</button></a>
+                    <a href="https://testbook-bd5cc.web.app/login" target="_blank" rel="noreferrer"><button className="btn btn-primary">Page</button></a>
+                </div>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                className="d-block w-100"
+                src="https://github.com/Souto751/portfolio/blob/main/src/images/randomQuote.jpg?raw=true"
+                alt="Third slide"
+                />
+
+                <Carousel.Caption className="text">
+                <h3>Random Quote Generator</h3>
+                <p>This is a random developers' quotes generator.</p>
+                <div className="buttons">
+                    <a href="https://codepen.io/Souto751/pen/gOLppbq" target="_blank" rel="noreferrer"><button className="btn btn-secondary">Repo</button></a>
+                    <a href="https://codepen.io/Souto751/full/gOLppbq" target="_blank" rel="noreferrer"><button className="btn btn-primary">Page</button></a>
+                </div>
+                </Carousel.Caption>
+            </Carousel.Item>
+            </Carousel>
+    )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Header() {
+    return (
+        <div className="header container-fluid">
+            <h1 className="title"><a href="https://github.com/Souto751" className="link">Souto751</a></h1>
+        </div>
+    )
+}
